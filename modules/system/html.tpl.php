6f4479cebef8fc650fb1c8b61998820c0f29b551
nagy <nagy.norbert1989@gmail.com>
2016-05-05 21:03:47 +0300

    System bug with new theme fixed

--- modules/system/html.tpl.php
+++ modules/system/html.tpl.php
@@ -57,7 +57,12 @@
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
-  <?php print $page; ?>
+  <?php
+    if (is_array($page)) {
+      $page = render($page);
+    }
+    print $page;
+  ?>
   <?php print $page_bottom; ?>
 </body>
 </html>
